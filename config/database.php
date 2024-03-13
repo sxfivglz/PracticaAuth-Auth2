a7b52a37705a93f34dad6600c88b0dbff85b5bfa
sofiagonzalez <sofiiaglz@hotmail.com>
2024-03-13 00:13:44 -0600

    postgresql conf listo

--- config/database.php
+++ config/database.php
@@ -75,8 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            // 'sslmode' => 'prefer',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'verify-full',
         ],
 
         'sqlsrv' => [
